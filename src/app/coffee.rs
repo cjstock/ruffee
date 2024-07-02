6fa83c3a2980b8c061a11408c2535849cc278b00
Corey Stock <coreystock@gmail.com>
2024-07-01 22:43:14 -0700

    Figured out some routing and began iterating on coffees ui

--- src/app/coffee.rs
+++ src/app/coffee.rs
@@ -1,20 +1,38 @@
 use askama::Template;
-use axum::{extract::State, routing::get, Router};
-use serde::Serialize;
+use axum::{
+    debug_handler,
+    extract::State,
+    routing::{get, post},
+    Form, Router,
+};
+use serde::{Deserialize, Serialize};
+use uuid::Uuid;
 
 use crate::{AppState, Result};
 
 pub(crate) fn coffee_router(app_state: AppState) -> Router {
     Router::new()
         .route("/", get(get_coffees))
+        .route("/add", get(page_add_coffee).post(add_coffee))
         .with_state(app_state)
 }
 
-#[derive(Serialize, sqlx::FromRow)]
+#[derive(Serialize, sqlx::FromRow, Deserialize)]
 struct Coffee {
+    coffee_id: Uuid,
     title: String,
-    altitude: Option<i64>,
+    altitude: Option<i32>,
     description: Option<String>,
+    country: Option<String>,
+    region: Option<String>,
+    farm: Option<String>,
+    farmer: Option<String>,
+    variety: Option<String>,
+    process: Option<String>,
+    grade: Option<String>,
+    roast_level: Option<String>,
+    tasting_notes: Option<Vec<String>>,
+    recommended_brew_methods: Option<Vec<String>>,
 }
 
 #[derive(Template)]
@@ -28,4 +46,33 @@ async fn get_coffees(State(app_state): State<AppState>) -> Result<CoffeesTemplat
         .fetch_all(&app_state.db)
         .await?;
     Ok(CoffeesTemplate { coffees })
+}
+
+#[derive(Template)]
+#[template(path = "coffee.html")]
+struct CoffeeCardTemplate {
+    coffee: Coffee,
+}
+
+#[debug_handler]
+async fn add_coffee(
+    State(app_state): State<AppState>,
+    Form(coffee): Form<Coffee>,
+) -> Result<CoffeeCardTemplate> {
+    let result = sqlx::query!(r#"
+        insert into coffee
+        (title, altitude, description, country, region, farm, farmer, variety, process, grade, roast_level, tasting_notes, recommended_brew_methods) values
+        ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11, $12, $13)
+        returning coffee_id
+    "#, coffee.title, coffee.altitude, coffee.description, coffee.country, coffee.region, coffee.farm, coffee.farmer, coffee.variety, coffee.process, coffee.grade, coffee.roast_level, coffee.tasting_notes.as_deref(), coffee.recommended_brew_methods.as_deref()).fetch_one(&app_state.db).await?;
+    tracing::trace!("coffee added with id: {:?}", result);
+    Ok(CoffeeCardTemplate { coffee })
+}
+
+#[derive(Template)]
+#[template(path = "add_coffee.html")]
+struct PageAddCoffee {}
+
+async fn page_add_coffee() -> Result<PageAddCoffee> {
+    Ok(PageAddCoffee {})
 }
--- src/app/router.rs
+++ src/app/router.rs
@@ -1,8 +1,6 @@
-use axum::Router;
-
-use crate::AppState;
-
 use super::coffee::coffee_router;
+use crate::AppState;
+use axum::Router;
 
 pub fn app_router(app_state: AppState) -> Router {
     Router::new().nest("/coffees", coffee_router(app_state))
